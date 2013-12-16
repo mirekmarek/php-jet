8e6bad8dba63228a3ab5752c0cce034066bf0d00
Mirek Marek <mirek.marek.2m@gmail.com>
2013-12-16 20:05:24 +0100

    BSD license

--- application/initXHProf.php
+++ application/initXHProf.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ *
+ * @copyright Copyright (c) 2011-2013 Miroslav Marek <mirek.marek.2m@gmail.com>
+ * @license http://www.php-jet.net/php-jet/license.txt
+ * @author Miroslav Marek <mirek.marek.2m@gmail.com>
+ * @version <%VERSION%>
+ *
+ * @category Jet
+ * @package application bootstrap
+ */
 if( ($xhprof_available = extension_loaded("xhprof")) ){
 	xhprof_enable();
 
