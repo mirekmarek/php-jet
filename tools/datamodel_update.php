8adef8991ee46006123b015c320f3c17b2efb25c
Mirek Marek <mirek.marek.2m@gmail.com>
2013-12-16 20:03:43 +0100

    BSD license

--- tools/datamodel_update.php
+++ tools/datamodel_update.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ *
+ * @copyright Copyright (c) 2011-2013 Miroslav Marek <mirek.marek.2m@gmail.com>
+ * @license http://www.php-jet.net/php-jet/license.txt
+ * @author Miroslav Marek <mirek.marek.2m@gmail.com>
+ * @version <%VERSION%>
+ *
+ * @category Jet
+ * @package tools
+ */
 namespace Jet;
 require "includes/bootstrap_cli.php";
 
