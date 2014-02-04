5ac63ad3e32d9bbb9fef2f94415bcd785c1bfe82
Mirek Marek <mirek.marek.2m@gmail.com>
2014-02-04 18:30:18 +0100

    Refactoring ...

---
+++ _tests/application-test-config.php
@@ -0,0 +1,16 @@
+<?php
+ return array(
+	'database' => array(
+		'default_connection_name' => 'none',
+		'connections' => array(
+			'test_mysql' => array(
+				'name' => 'test_mysql',
+				'driver' => 'mysql',
+				'DSN' => 'host=127.0.0.1;dbname=jet;charset=utf8',
+				'username' => 'jet',
+				'password' => 'jet'
+			)
+		)
+	),
+);
+;
---
+++ application/_installer/dictionaries/MvcRouter/en_US.php
@@ -0,0 +1,34 @@
+<?php
+ return array(
+	'4d695f93ec821778d1ee2b39895e7d41' => array(
+		'phrase' => 'Router configuration',
+		'translation' => '',
+		'is_translated' => false,
+	),
+	'b3a19f7207588e51541fa19d145267d6' => array(
+		'phrase' => 'Enable cache: ',
+		'translation' => '',
+		'is_translated' => false,
+	),
+	'671ca6a8ef6324885525456c886ca486' => array(
+		'phrase' => 'Cache backend type: ',
+		'translation' => '',
+		'is_translated' => false,
+	),
+	'1c52f93af165b8157dfae98d23886237' => array(
+		'phrase' => 'Default site UI manager module: ',
+		'translation' => '',
+		'is_translated' => false,
+	),
+	'e17871780659d3b269dbedf2fba4e767' => array(
+		'phrase' => 'Default administration UI manager module: ',
+		'translation' => '',
+		'is_translated' => false,
+	),
+	'fc9e4b9ccd576c822c989d379631d58b' => array(
+		'phrase' => 'Authentication and authorization manager module: ',
+		'translation' => '',
+		'is_translated' => false,
+	),
+);
+;
