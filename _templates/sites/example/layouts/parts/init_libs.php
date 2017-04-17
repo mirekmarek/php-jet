3a0f865a1b437439ba9c66f2975f07bd12fc8d09
Mirek Marek <mirek.marek.2m@gmail.com>
2017-04-17 14:20:48 +0200

    ...

---
+++ _templates/sites/example/layouts/parts/init_libs.php
@@ -0,0 +1,10 @@
+<?php
+use Jet\Mvc_Layout;
+
+/**
+ * @var Mvc_Layout $this
+ */
+
+$this->requireCssFile( '//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css' );
+$this->requireCssFile( '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+$this->requireCssFile( '%JET_PUBLIC_URI%styles/flags.css' );
