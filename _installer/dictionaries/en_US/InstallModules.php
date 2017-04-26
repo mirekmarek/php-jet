d30680227249142d983d02324615d80f68f18003
Mirek Marek <mirek.marek.2m@gmail.com>
2017-04-26 19:27:34 +0200

    ...

---
+++ _installer/dictionaries/en_US/InstallModules.php
@@ -0,0 +1,4 @@
+<?php
+return [
+	'Modules installation' => '',
+];
---
+++ _installer/dictionaries/en_US/Welcome.php
@@ -0,0 +1,7 @@
+<?php
+return [
+	'Welcome!' => '',
+	'Please select locale' => '',
+	'Please select locale: ' => '',
+	'Welcome' => '',
+];
