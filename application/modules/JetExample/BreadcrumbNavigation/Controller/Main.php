7913f1754f5bcb07fe8c191f6ee54c5c51ee1404
Mirek Marek <mirek.marek.2m@gmail.com>
2017-05-24 17:56:00 +0200

    ...

--- application/modules/JetExample/BreadcrumbNavigation/Controller/Main.php
+++ application/modules/JetExample/BreadcrumbNavigation/Controller/Main.php
@@ -2,7 +2,7 @@
 /**
  *
  * @copyright Copyright (c) 2011-2017 Miroslav Marek <mirek.marek.2m@gmail.com>
- * @license http://www.php-jet.net/php-jet/license.txt
+ * @license http://www.php-jet.net/license/license.txt
  * @author Miroslav Marek <mirek.marek.2m@gmail.com>
  */
 namespace JetApplicationModule\JetExample\BreadcrumbNavigation;
