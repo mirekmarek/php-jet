f20d580ff91bb677a30642bd8e31f09bd782b596
Mirek Marek <mirek.marek.2m@gmail.com>
2017-06-02 12:23:38 +0200

    ...

---
+++ application/dictionaries/cs_CZ/JetExample.System.Visitors.Roles.php
@@ -0,0 +1,28 @@
+<?php
+return [
+	'Visitors - User roles'                    => 'Návštěvníci - Uživatelské role',
+	'ID'                                       => 'ID',
+	'Name'                                     => 'Název',
+	'Description'                              => 'Popis',
+	'Create a new Role'                        => 'Vytvořit novou roli',
+	'Please enter a name'                      => 'Prosím zadejte název',
+	'Invalid value'                            => 'Neplatná hodnota',
+	'Privileges'                               => 'Oprávnění',
+	'Users'                                    => 'Uživatelé',
+	'Username'                                 => 'Uživatelské jméno',
+	'Edit role <b>%ROLE_NAME%</b>'             => 'Upravit roli <b>%ROLE_NAME%</b>',
+	'Role <b>%ROLE_NAME%</b> has been created' => 'Role <b>%ROLE_NAME%</b> byla vytvořena',
+	'Role <b>%ROLE_NAME%</b> has been updated' => 'Role <b>%ROLE_NAME%</b> byla upravena',
+	'Role <b>%ROLE_NAME%</b> has been deleted' => 'Role <b>%ROLE_NAME%</b> byla smazána',
+	'Role detail <b>%ROLE_NAME%</b>'           => 'Detail role <b>%ROLE_NAME%</b>',
+	'Delete role <b>%ROLE_NAME%</b>'           => 'Smazat roli <b>%ROLE_NAME%</b>',
+	'Do you really want to delete this role?'  => 'Opravdu si přejete smazat tuto roli?',
+	'Sites and pages'                          => 'Weby a stránky',
+
+
+	'Visitor roles management'                 => 'Správa rolí návštěvníků',
+	'Get role(s) data'                         => 'Získat informace o rolích',
+	'Add new role'                             => 'Přidat novou roli',
+	'Update role'                              => 'Upravit roli',
+	'Delete role'                              => 'Smazat roli',
+];
