ede8fdee75b65eee53bd1ec9bc681cf1b3d2ee79
Mirek Marek <mirek.marek.2m@gmail.com>
2017-06-25 12:32:57 +0200

    ...

--- application/Modules/JetExample/WebUI/Controller/Main.php
+++ application/Modules/JetExample/WebUI/Controller/Main.php
@@ -5,7 +5,7 @@
  * @license http://www.php-jet.net/license/license.txt
  * @author Miroslav Marek <mirek.marek.2m@gmail.com>
  */
-namespace JetApplicationModule\JetExample\SiteUI;
+namespace JetApplicationModule\JetExample\WebUI;
 
 use Jet\Mvc_Controller_Default;
 use Jet\Mvc;
