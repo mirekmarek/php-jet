2d64f717f86639dc189365a09faa41c433e9884f
Mirek Marek <mirek@mirek-laptop>
2020-11-08 20:36:59 +0100

    ...

--- _tools/includes/bootstrap_cli.php
+++ _tools/includes/bootstrap_cli.php
@@ -10,11 +10,11 @@ namespace Jet;
 
 $application_dir = dirname(dirname(__DIR__)) . '/application/';
 
-require_once($application_dir . 'config/paths.php');
+require_once($application_dir . 'config/PATH.php');
 require_once($application_dir . 'config/jet.php');
 
 
-$init_dir = JET_PATH_APPLICATION.'Init/';
+$init_dir = PATH::APPLICATION().'Init/';
 require( $init_dir.'Autoloader.php');
 require( $init_dir.'ClassNames.php' );
 
