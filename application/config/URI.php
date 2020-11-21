d3eff70b09d358e0b13233eba644e207908f8069
Mirek Marek <mirek@mirek-laptop>
2020-11-21 06:30:25 +0100

    ...

--- application/config/URI.php
+++ application/config/URI.php
@@ -6,7 +6,7 @@ require_once SysConf_PATH::LIBRARY().'Jet/SysConf/URI.php';
 
 $base_URI = '/';
 
-//- It is better to hardcode value on the production system
+//- It is better to hardcode value for the production system
 $request_URI = isset( $_SERVER['REQUEST_URI'] ) ? $_SERVER['REQUEST_URI'] : '/';
 if( ($pos=strpos($request_URI, '?'))!==false ) {
 	$request_URI = substr($request_URI, 0, $pos-1);
