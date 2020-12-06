d0a1828c79f1e195bc9790b6efba8bc5d694c9e9
Mirek Marek <mirek@mirek-laptop>
2020-12-06 06:32:29 +0100

    ...

---
+++ _tools/studio/application/dictionaries/cs_CZ/login.php
@@ -0,0 +1,13 @@
+<?php
+return [
+	'Sign In'                                       => 'Přihlásit se ',
+	'Please enter username'                         => 'Prosím zadejte uživatelské jméno',
+	'Please enter password'                         => 'Prosím zadejte heslo',
+	'username'                                      => 'uživatelské jméno',
+	'password'                                      => 'heslo',
+	'Logout'                                        => 'Odhlásit se',
+	'The access configuration is corrupted'         => 'Konfigurační soubor s přístupovými udáji je poškozen',
+	'The access configuration file is not readable' => 'Konfigurační soubor s přístupovými udáji není čitelný',
+	'The access configuration file does not exist'  => 'Konfigurační soubor s přístupovými udáji neexistuje',
+	'Invalid username or password!'                 => 'Nesprávné uživatelské jméno, nebo heslo',
+];
