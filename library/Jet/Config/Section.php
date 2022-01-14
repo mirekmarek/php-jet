c840186a652e02e0e84fde78d14c5e9c67668a51
mirekmarek <mirek.marek.2m@gmail.com>
2022-01-14 14:36:48 +0100

    bug fixes

--- library/Jet/Config/Section.php
+++ library/Jet/Config/Section.php
@@ -25,7 +25,7 @@ class Config_Section extends Config
 	protected ?Config_Definition_Config_Section $definition = null;
 
 
-	/** @noinspection PhpMissingParentConstructorInspection */
+
 	/**
 	 * @param array|null $data
 	 */
@@ -37,9 +37,9 @@ class Config_Section extends Config
 	}
 
 	/**
-	 * @return Config_Definition_Config
+	 * @return Config_Definition_Config_Section
 	 */
-	public function getDefinition(): Config_Definition_Config
+	public function getDefinition(): Config_Definition_Config_Section
 	{
 		if( !$this->definition ) {
 			$this->definition = Config_Definition::getSectionConfigDefinition( static::class );
