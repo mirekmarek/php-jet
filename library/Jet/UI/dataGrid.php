bb4466a1da1118a57e94bcb0ac70ac7d9dfe7f0c
mirekmarek <mirek.marek.2m@gmail.com>
2022-09-23 14:19:41 +0200

    UI_dataGrid->hasColumn method added

--- library/Jet/UI/dataGrid.php
+++ library/Jet/UI/dataGrid.php
@@ -160,7 +160,19 @@ class UI_dataGrid extends UI_Renderer_Single
 
 		return $creator( $column_name, $desc );
 	}
-
+	
+	
+	/**
+	 * @param string $name
+	 *
+	 * @return bool
+	 */
+	public function hasColumn( string $name ) : bool
+	{
+		return isset($this->columns[$name]);
+	}
+	
+	
 	/**
 	 * @param string $name
 	 *
