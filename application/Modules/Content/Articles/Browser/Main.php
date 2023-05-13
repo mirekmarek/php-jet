d0fee578f51696d484f9845e747f86c83273fbb2
mirekmarek <mirek.marek.2m@gmail.com>
2023-05-13 11:21:10 +0200

    ...

--- application/Modules/Content/Articles/Browser/Main.php
+++ application/Modules/Content/Articles/Browser/Main.php
@@ -9,34 +9,10 @@
 namespace JetApplicationModule\Content\Articles\Browser;
 
 use Jet\Application_Module;
-use Jet\MVC_Page_Interface;
-use JetApplication\Content_Article_Localized;
-use JetApplication\PageGenerator_URLProvider;
 
 /**
  *
  */
-class Main extends Application_Module implements PageGenerator_URLProvider
+class Main extends Application_Module
 {
-	
-	public function getPageURLs( MVC_Page_Interface $page ): array
-	{
-		$articles = Content_Article_Localized::fetchInstances([
-			'locale' => $page->getLocale()
-		]);
-		
-		$URLs = [];
-		
-		$pg_count = ceil( count($articles) / 20);
-		for( $p=2 ; $p<=$pg_count ; $p++) {
-			$URLs[] = 'page:'.$p.'/';
-		}
-		
-		
-		foreach($articles as $article) {
-			$URLs[] = $article->getURIFragment();
-		}
-		
-		return $URLs;
-	}
 }
