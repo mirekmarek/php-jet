aaa11c18e25ce252f07fe6fa911e57bde128fea7
mirekmarek <mirek.marek.2m@gmail.com>
2023-05-19 08:03:08 +0200

    Unification of settings with other pages

--- application/bases/web/pages/cs_CZ/podstranka/1/page_data.php
+++ application/bases/web/pages/cs_CZ/podstranka/1/page_data.php
@@ -1,20 +1,30 @@
 <?php
+$content = require __DIR__ . '/../common_content.php';
+
 return [
-	'id'                 => 'subpage_1',
-	'order'              => 1,
-	'name'               => 'Podstránka 1',
-	'is_active'          => true,
-	'SSL_required'       => false,
-	'title'              => 'Podstránka 1',
-	'icon'               => '',
-	'menu_title'         => 'Podstránka 1',
-	'breadcrumb_title'   => 'Podstránka 1',
-	'is_secret'          => false,
-	'http_headers'       => [
-	],
+	'id' => 'subpage_1',
+	'order' => 1,
+	'name' => 'Podstránka 1',
+	'title' => 'Podstránka 1',
+	'menu_title' => 'Podstránka 1',
+	'breadcrumb_title' => 'Podstránka 1',
 	'layout_script_name' => 'default',
-	'meta_tags'          => [
+	'meta_tags' => [
+		[
+			'attribute'   => 'Meta1attribute',
+			'attribute_value' => 'Meta 1 attribute value',
+			'content' => 'Meta 1 content'
+		],
+		[
+			'attribute'   => 'Meta2attribute',
+			'attribute_value' => 'Meta 2 attribute value',
+			'content' => 'Meta 2 content'
+		],
+		[
+			'attribute'   => 'Meta3attribute',
+			'attribute_value' => 'Meta 3 attribute value',
+			'content' => 'Meta 3 content'
+		],
 	],
-	'contents'           => [
-	],
+	'contents' => $content
 ];
