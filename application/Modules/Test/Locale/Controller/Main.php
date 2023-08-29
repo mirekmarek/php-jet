fe7125c053c830665ea2895a486f4b507ccc54cc
mirekmarek <mirek.marek.2m@gmail.com>
2023-08-29 15:14:30 +0200

    Test.Locale module refactored It is now possible to select each locale to test.

--- application/Modules/Test/Locale/Controller/Main.php
+++ application/Modules/Test/Locale/Controller/Main.php
@@ -8,6 +8,8 @@
 
 namespace JetApplicationModule\Test\Locale;
 
+use Jet\Http_Request;
+use Jet\Locale;
 use Jet\MVC_Controller_Default;
 
 /**
@@ -20,6 +22,16 @@ class Controller_Main extends MVC_Controller_Default
 	 */
 	public function test_locale_Action(): void
 	{
+		$locale = Http_Request::GET()->getString(
+			key: 'locale',
+			default_value: Locale::getCurrentLocale()->toString(),
+			valid_values: array_keys(Locale::getAllLocalesList())
+		);
+		
+		$locale = new Locale($locale);
+		
+		$this->view->setVar('locale', $locale);
+		
 		$this->output( 'test-locale' );
 	}
 }
