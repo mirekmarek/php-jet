8ee7b8e284a8234136dee702bfa49402c5da620d
mirekmarek <mirek.marek.2m@gmail.com>
2023-10-01 21:02:44 +0200

    Dictionaries updated

--- application/Modules/Test/ORM/Model/A1.php
+++ application/Modules/Test/ORM/Model/A1.php
@@ -11,6 +11,10 @@ namespace JetApplicationModule\Test\ORM;
 use Jet\DataModel;
 use Jet\DataModel_Definition;
 use Jet\DataModel_IDController_UniqueString;
+use Jet\Form;
+use Jet\Form_Field;
+use Jet\DataModel_Fetch_Instances;
+use Jet\Data_DateTime;
 
 /**
  *
@@ -43,6 +47,240 @@ class Model_A1 extends DataModel
 		data_model_class: Model_A1_1toN::class
 	)]
 	protected array $related_1toN = [];
+
+	/**
+	 * @var string
+	 */ 
+	#[DataModel_Definition(
+		type: DataModel::TYPE_STRING,
+		max_len: 255
+	)]
+	protected string $aaa = '';
+
+	/**
+	 * @var string
+	 */ 
+	#[DataModel_Definition(
+		type: DataModel::TYPE_STRING,
+		max_len: 255
+	)]
+	protected string $ccc = '';
+
+	/**
+	 * @var string
+	 */ 
+	#[DataModel_Definition(
+		type: DataModel::TYPE_STRING,
+		max_len: 255
+	)]
+	protected string $eee = '';
+
+	/**
+	 * @var string
+	 */ 
+	#[DataModel_Definition(
+		type: DataModel::TYPE_STRING,
+		max_len: 255
+	)]
+	protected string $fff = '';
+
+	/**
+	 * @var string
+	 */ 
+	#[DataModel_Definition(
+		type: DataModel::TYPE_STRING,
+		max_len: 255
+	)]
+	protected string $a1 = '';
+
+	/**
+	 * @var string
+	 */ 
+	#[DataModel_Definition(
+		type: DataModel::TYPE_STRING,
+		max_len: 255
+	)]
+	protected string $a2 = '';
+
+	/**
+	 * @var string
+	 */ 
+	#[DataModel_Definition(
+		type: DataModel::TYPE_STRING,
+		max_len: 255
+	)]
+	protected string $b1 = '';
+
+	/**
+	 * @var string
+	 */ 
+	#[DataModel_Definition(
+		type: DataModel::TYPE_STRING,
+		max_len: 255
+	)]
+	protected string $b2 = '';
+
+	/**
+	 * @var string
+	 */ 
+	#[DataModel_Definition(
+		type: DataModel::TYPE_STRING,
+		max_len: 255
+	)]
+	protected string $c1 = '';
+
+	/**
+	 * @var string
+	 */ 
+	#[DataModel_Definition(
+		type: DataModel::TYPE_STRING,
+		max_len: 255
+	)]
+	protected string $c2 = '';
+
+	/**
+	 * @var ?Data_DateTime
+	 */ 
+	#[DataModel_Definition(
+		type: DataModel::TYPE_DATE
+	)]
+	protected ?Data_DateTime $c3 = null;
+
+	/**
+	 * @var ?Data_DateTime
+	 */ 
+	#[DataModel_Definition(
+		type: DataModel::TYPE_DATE_TIME
+	)]
+	protected ?Data_DateTime $c4 = null;
+
+	/**
+	 * @param string $value
+	 */
+	public function setEee( string $value ) : void
+	{
+		$this->eee = $value;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getEee() : string
+	{
+		return $this->eee;
+	}
+
+	/**
+	 * @param string $value
+	 */
+	public function setFff( string $value ) : void
+	{
+		$this->fff = $value;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getFff() : string
+	{
+		return $this->fff;
+	}
+
+	/**
+	 * @param string $value
+	 */
+	public function setB1( string $value ) : void
+	{
+		$this->b1 = $value;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getB1() : string
+	{
+		return $this->b1;
+	}
+
+	/**
+	 * @param string $value
+	 */
+	public function setB2( string $value ) : void
+	{
+		$this->b2 = $value;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getB2() : string
+	{
+		return $this->b2;
+	}
+
+	/**
+	 * @param string $value
+	 */
+	public function setC1( string $value ) : void
+	{
+		$this->c1 = $value;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getC1() : string
+	{
+		return $this->c1;
+	}
+
+	/**
+	 * @param string $value
+	 */
+	public function setC2( string $value ) : void
+	{
+		$this->c2 = $value;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getC2() : string
+	{
+		return $this->c2;
+	}
+
+	/**
+	 * @param Data_DateTime|string|null $value
+	 */
+	public function setC3( Data_DateTime|string|null $value ) : void
+	{
+		$this->c3 = Data_DateTime::catchDate( $value );
+	}
+
+	/**
+	 * @return Data_DateTime|null
+	 */
+	public function getC3() : Data_DateTime|null
+	{
+		return $this->c3;
+	}
+
+	/**
+	 * @param Data_DateTime|string|null $value
+	 */
+	public function setC4( Data_DateTime|string|null $value ) : void
+	{
+		$this->c4 = Data_DateTime::catchDateTime( $value );
+	}
+
+	/**
+	 * @return Data_DateTime|null
+	 */
+	public function getC4() : Data_DateTime|null
+	{
+		return $this->c4;
+	}
 
 
 
