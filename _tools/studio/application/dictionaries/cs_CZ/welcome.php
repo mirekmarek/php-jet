a4818b9783f17e24d57c3730b59d444476820e26
mirekmarek <mirek.marek.2m@gmail.com>
2023-10-27 12:54:14 +0200

    JetStudio: * documentation link added * questions link added

---
+++ _tools/studio/application/dictionaries/cs_CZ/welcome.php
@@ -0,0 +1,7 @@
+<?php
+return [
+	'PHP Jet Documentation' => 'Dokumentace PHP Jet',
+	'Ask a question' => 'Ptejte se',
+	'https://www.php-jet.net/doc/' => 'https://www.php-jet.net/cs/doc/',
+	'https://questions.php-jet.net/' => '',
+];
---
+++ _tools/studio/application/dictionaries/en_US/welcome.php
@@ -0,0 +1,7 @@
+<?php
+return [
+	'Ask a question' => '',
+	'PHP Jet Documentation' => '',
+	'https://www.php-jet.net/doc/' => '',
+	'https://questions.php-jet.net/' => '',
+];
